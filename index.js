7e6e62605bcfa16fd868dccf91a5c51407818005
rajatvijay <rajatvijay5@gmail.com>
2018-06-23 18:30:24 +0530

    get all the public self owned repos from the github apis

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 #!/usr/bin/env node
 
 // console.log ('Hello Shell!');
+const dotenv = require ('dotenv');
+dotenv.load ();
 
 const program = require ('commander');
+const axios = require ('axios');
+
+const GITHUB_BASE_URL = 'https://api.github.com';
+
+async function getPublicRepos (username) {
+  const response = await axios ({
+    method: 'GET',
+    url: `/users/${username}/repos`,
+    baseURL: GITHUB_BASE_URL,
+  });
+  return response.data;
+}
+
+async function getPublicSelfOwnedRepos (username) {
+  const repos = await getPublicRepos (username);
+  return repos.filter (r => r.owner.login === username);
+}
 
 program
   .arguments ('<docURL>')
   .action (function (docURL) {
-    console.log (docURL);
-    console.log (process.env);
+    const {GITHUB_USERNAME, GITHUB_PASSWORD, PAPER_API} = process.env;
+
+    getPublicSelfOwnedRepos (GITHUB_USERNAME).then ();
+    // console.log
   })
   .parse (process.argv);
